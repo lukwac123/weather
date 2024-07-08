eacfaa372c8b303180ba48bb32bad7c4e9d2a910
lukwac123 <lukasz-waclawek@wp.pl>
2024-07-08 13:39:02 +0200

    Dodanie drugiej serii danych z najniższą temperaturą dnia.

--- sitka_highs.py
+++ sitka_highs.py
@@ -10,21 +10,24 @@ lines = path.read_text().splitlines()
 reader = csv.reader(lines)
 header_row = next(reader)
 
-# Pobranie dat i temperatur maksymalnych z pliku.
-dates, highs = [], []
+# Pobranie dat i temperatur maksymalnych i minimalnych z pliku.
+dates, highs, lows = [], [], []
 for row in reader:
     current_date = datetime.strptime(row[2], "%Y-%m-%d")
     high = int(row[4])
+    low = int(row[5])
     dates.append(current_date)
     highs.append(high)
+    lows.append(low)
 
 # Dane wykresu.
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
 ax.plot(dates, highs, color='red')
+ax.plot(dates, lows, color='blue')
 
 # Formatowanie wykresu.
-ax.set_title("Najwyższa temperatura dnia - 2021", fontsize=24)
+ax.set_title("Najwyższa i najniższa temperatura dnia - 2021", fontsize=24)
 ax.set_xlabel('', fontsize=16)
 fig.autofmt_xdate()
 ax.set_ylabel('Temperatura (F)', fontsize=16)
