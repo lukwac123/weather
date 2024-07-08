e86182149a8d2463e967354760656d9120209ced
lukwac123 <lukasz-waclawek@wp.pl>
2024-07-08 13:20:26 +0200

    Wyświetlanie daty.

--- sitka_highs.py
+++ sitka_highs.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import csv
+from datetime import datetime
 
 import matplotlib.pyplot as plt
 
@@ -9,20 +10,23 @@ lines = path.read_text().splitlines()
 reader = csv.reader(lines)
 header_row = next(reader)
 
-# Pobranie temperatur maksymalnych z pliku.
-highs = []
+# Pobranie dat i temperatur maksymalnych z pliku.
+dates, highs = [], []
 for row in reader:
+    current_date = datetime.strptime(row[2], "%Y-%m-%d")
     high = int(row[4])
+    dates.append(current_date)
     highs.append(high)
 
 # Dane wykresu.
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
-ax.plot(highs, color='red')
+ax.plot(dates, highs, color='red')
 
 # Formatowanie wykresu.
 ax.set_title("Najwyższa temperatura dnia, lipiec 2021", fontsize=24)
 ax.set_xlabel('', fontsize=16)
+fig.autofmt_xdate()
 ax.set_ylabel('Temperatura (F)', fontsize=16)
 ax.tick_params(labelsize=16)
 
